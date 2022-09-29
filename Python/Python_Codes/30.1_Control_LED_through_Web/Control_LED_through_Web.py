82e35ec4cd0818aaeabd1347d2030fe0d5a5063d
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2022-09-29 10:41:33 +0800

    Update

--- Python/Python_Codes/30.1_Control_LED_through_Web/Control_LED_through_Web.py
+++ Python/Python_Codes/30.1_Control_LED_through_Web/Control_LED_through_Web.py
@@ -26,13 +26,27 @@ def WebPage():
         gpio_state = 'ON'
     
     # html code ...
-    html = """<html><head> <title>ESP8266 Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #4286f4; border: none; 
-  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP8266 Web Server</h1> 
-  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+    html = """
+    <html>
+        <head>
+            <title>ESP8266 Web Server</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" href="data:,">
+            <style>
+                html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+                h1{color: #0F3376; padding: 2vh;}
+                p{font-size: 1.5rem;}
+                button{display: inline-block; background-color: #4286f4; border: none;border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+                button2{background-color: #4286f4;}
+            </style>
+        </head>
+        <body> <h1>ESP8266 Web Server</h1> 
+          <p>GPIO state: <strong>""" + gpio_state + """</strong></p>
+          <p><a href="/?led=on"><button class="button">ON</button></a></p>
+          <p><a href="/?led=off"><button class="button button2">OFF</button></a></p>
+        </body>
+    </html>
+    """
     return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
