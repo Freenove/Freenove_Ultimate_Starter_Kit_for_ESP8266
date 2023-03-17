a2233e0c28d6d656131e540a377de5e01163f3cb
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-03-17 10:49:43 +0800

    Update Sketch_24.2_Temperature_and_Humidity_Sensor.ino

--- C/Sketches/Sketch_24.2_Temperature_and_Humidity_Sensor/Sketch_24.2_Temperature_and_Humidity_Sensor.ino
+++ C/Sketches/Sketch_24.2_Temperature_and_Humidity_Sensor/Sketch_24.2_Temperature_and_Humidity_Sensor.ino
@@ -2,7 +2,7 @@
   Filename    : Temperature and Humidity Sensor
   Description : Use DHT11 to measure temperature and humidity.Print the result to the LCD1602.
   Auther      : www.freenove.com
-  Modification: 2022/05/11
+  Modification: 2020/07/11
 **********************************************************************/
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
@@ -17,6 +17,9 @@ int dhtPin = 5;                  // the number of the DHT11 sensor pin
 
 void setup() {
   Wire.begin(SDA, SCL);           // attach the IIC pin
+  if (!i2CAddrTest(0x27)) {
+    lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+  }
   lcd.init();                     // LCD driver initialization
   lcd.backlight();                // Open the backlight
   dht.setup(dhtPin, DHTesp::DHT11); //attach the dht pin and initialize it
@@ -33,6 +36,15 @@ void loop() {
   lcd.print(DHT.temperature);   
   lcd.setCursor(0, 1);              //set the cursor to column 0, line 0 
   lcd.print("Humidity   :");        //display the Humidity on the LCD1602
-  lcd.print(DHT.humidity); 
-  delay(2000); 
+  lcd.print(DHT.humidity);  
+  delay(2000);
+}
+
+bool i2CAddrTest(uint8_t addr) {
+  Wire.begin();
+  Wire.beginTransmission(addr);
+  if (Wire.endTransmission() == 0) {
+    return true;
+  }
+  return false;
 }
